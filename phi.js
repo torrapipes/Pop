4f7f94e15b39064553f39e99ab26c80e63832699
Eyrenna <claudia365x@gmail.com>
2019-10-15 17:51:38 +0200

    Corrección divisor

--- phi.js
+++ phi.js
@@ -6,7 +6,7 @@
  * **/
 
 
-function phi(matriu){
+function phi(matriu) {
 
     var event00 = matriu[0][0]; // NO pop, NO x
     var event01 = matriu[0][1]; // NO pop, SI x
@@ -17,7 +17,7 @@ function phi(matriu){
     var dividendo = event11 * event00 - event10 * event01;
 
     // (NO pop + NO pop) * (NO pizza + NO pizza) * (SI pop + SI pop) * (SI pizza + SI pizza)
-    var divisor = Math.sqrt((event00 + event01) * (event00 + event10) * (event10 + event10) * (event11 + event01));
+    var divisor = Math.sqrt((event01 + event11) * (event00 + event10) * (event10 + event11) * (event00 + event01));
 
     var phi = dividendo / divisor;
 
@@ -27,6 +27,9 @@ function phi(matriu){
 
 
 // Cas test
-var matriu = [ [76, 9], [4, 1] ];
+var matriu = [
+    [76, 9],
+    [4, 1]
+];
 
 console.log(phi(matriu));
