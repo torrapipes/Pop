e6695462dbe96cb7941f281b4da595ca019d1f66
Eyrenna <claudia365x@gmail.com>
2019-12-07 12:45:44 +0100

    Feat: Reestructuración front

---
+++ pages/head.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html
+<head>
+    <meta charset="utf-8">
+    <title><?php echo $page_title?></title>
+    <link rel="icon" type="image/png" href="../img/octopus.png">
+    <link rel="stylesheet" href="../css/general.css" >
+
+</head>
+</html>
+
---
+++ pages/index.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html>
+<?php
+include_once "head.php";
+?>
+<body>
+    <div id="bg">
+
+    </div>
+</body>
+</html>
