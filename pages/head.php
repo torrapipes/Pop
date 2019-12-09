8f1e771004be78eeb52660be69b3da9023b553b1
torrapipes <crbergas@gmail.com>
2019-12-09 01:40:41 +0100

    feat: página login responsive

--- pages/head.php
+++ pages/head.php
@@ -2,6 +2,7 @@
 <html
     <head>
         <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title><?php echo $page_title?></title>
         <link rel="icon" type="image/png" href="../img/octopus.png">
         <link rel="stylesheet" href="../css/general.css" >
--- pages/index.php
+++ pages/index.php
@@ -10,21 +10,63 @@ include_once "head.php";
         <img src="../img/pulpo.png" id="pop"/>
     </div>
     <div id="login-box">
-        <form>
+        <form id="login-form" class="form">
             
             <div class="inputs">
                 <input type="text" name="usuario" placeholder="Usuario">
             </div>
 
             <div class="inputs">
-                <input type="password" name="password" placeholder="Password">
+                <input type="password" name="password" placeholder="Contraseña">
             </div>
 
             <div>
                 <input type="submit" name="enter" value="Entrar">
             </div>
-
+            <a href="#registro-box" class="trigger" id="login-text" class="text">¿Aún no te has registrado? Regístrate aquí </a>
         </form>
     </div>
+    <div id="registro-box">
+        <form id="registro-form" class="form">
+            
+            <div class="inputs">
+                <input type="text" name="usuario" placeholder="Usuario">
+            </div>
+
+            <div class="inputs">
+                <input type="password" name="password" placeholder="Contraseña">
+            </div>
+
+            <div>
+                <input type="submit" name="enter" value="Entrar">
+            </div>
+            <a href="#" id="registro-text" class="text">¿Ya tienes una cuenta? Inicia sesión </a>
+        </form>
+    </div>
+    <script type="text/javascript">
+
+        var login = document.getElementById("login-text");
+        var registro = document.getElementById("registro-text");
+
+        function toggleModalRegistro() {
+            document.getElementById("registro-form").classList.remove("show");
+            document.getElementById("registro-form").classList.add("hide");
+
+            document.getElementById("login-form").classList.remove("hide");
+            document.getElementById("login-form").classList.add("show");
+        }
+
+        function toggleModalLogin() {
+            document.getElementById("login-form").classList.remove("show");
+            document.getElementById("login-form").classList.add("hide");
+
+            document.getElementById("registro-form").classList.remove("hide");
+            document.getElementById("registro-form").classList.add("show");
+        }
+
+        registro.onclick = toggleModalRegistro;
+        login.onclick = toggleModalLogin;
+
+    </script>
 </body>
 </html>
