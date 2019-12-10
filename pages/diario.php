78960a7dde98f8c1360dc5f6f6fef7f0b04a0f5d
Eyrenna <claudia365x@gmail.com>
2019-12-10 01:31:54 +0100

    Feat: diario.php con img y css

--- pages/diario.php
+++ pages/diario.php
@@ -9,13 +9,31 @@ include_once "head.php";
     <?php
     include_once "header.php";
     ?>
-    <div>
         <?php
-        $diario = file_get_contents('../elements/diarioMariano.json');
-        $diario = json_encode($diario);
-        echo $diario{0};
+        $diarioJson = file_get_contents('../elements/diarioMariano.json');
+        $diario = json_decode($diarioJson, true);
+
+        echo '<div class="diario">';
+        for($mes=1; $mes <= sizeof($diario)/30; $mes++) {
+            $long = 30*$mes;
+            $diaActual = 1;
+            echo '<div class="mes">';
+                echo '<p class="mesTitle">Mes ' . $mes . '</p>';
+                for ($dia = $long-30; $dia < $long; $dia++) {
+                    echo '<p class="diaTitle"> Dia ' . $diaActual . '</p>';
+                    echo '<div class="diaContent"><div class="eventos">';
+                    foreach ($diario[$dia]['eventos'] as $evento) {
+                        echo '<p class="evento">' . $evento . '</p>';
+                    }
+                    $diaActual++;
+                    echo '</div>';
+                    $pulpo = ($diario[$dia]['pulpo']) ? 'pulpito.gif' : 'mariano.gif';
+                    echo '<div class="trans"><img src="../img/'. $pulpo . '"/></div></div>';
+                }
+            echo '</div>';
+        }
+        echo '</div>';
         ?>
-    </div>
 
     <?php
     include_once "footer.php";
